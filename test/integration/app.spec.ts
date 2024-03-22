5154f977c24c0ca0501696276c7376abcd6d25d5
Joseph Tsegen <tsegstech10@gmail.com>
2024-03-22 18:36:45 +0100

    feat: example logger

--- test/integration/app.spec.ts
+++ test/integration/app.spec.ts
@@ -6,8 +6,12 @@ import request from 'supertest';
 import { Env } from '../../src/internal/env';
 import { MODULE_TOKENS } from '../../src/config/ioc';
 import envValidator from '../../src/internal/env/env.validator';
+import { Logger } from '../../src/internal/logger';
+import { defaultSerializers } from '../../src/internal/logger/serializers';
 
 const container = new Container();
+
+const logger = new Logger({ name: 'eis', serializers: defaultSerializers() });
 
 container.bind<Env>(MODULE_TOKENS.Env).to(Env);
 
@@ -15,7 +19,7 @@ const env = container.get<Env>(MODULE_TOKENS.Env);
 
 env.load(envValidator);
 
-const app = new App(container, console).server.build();
+const app = new App(container, logger).server.build();
 
 describe('[GET] Health checker', () => {
   it('should successfully query the health checker', async () => {
